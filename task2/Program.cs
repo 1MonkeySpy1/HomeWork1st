59c1ee21ac97424a83e908e2cf7d0b04b0d29429
1MonkeySpy1 <sharondon2062@gmial.com>
2023-02-15 20:59:03 +0500

    Замено ошибок по нахождению макс. числа

--- task2/Program.cs
+++ task2/Program.cs
@@ -14,8 +14,9 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number c");
 int c = Convert.ToInt32(Console.ReadLine());
 
-if (a<b)  {max=b;}
-if (c>max) {max=c;}
+if (a>max)  {max=a;}
+if (b>max)  {max=b;}
+if (c>max)  {max=c;}
  
 Console.WriteLine("maximum=" + max);
  
